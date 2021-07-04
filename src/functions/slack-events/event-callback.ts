e68c2e4ec363b100f2aef7275940e38c8a75b804
Sam Chung <samc@seek.com.au>
2021-07-04 10:52:12 +1000

    feat: add date check for events

--- src/functions/slack-events/event-callback.ts
+++ src/functions/slack-events/event-callback.ts
@@ -1,16 +1,27 @@
 import {SlackCallbackEvent} from 'domain/slack';
+import {ReactionAddedEvent, ReactionRemovedEvent, ReactionMessageItem} from '@slack/bolt';
 import {logger} from 'lib';
 import {invokeAddUserReminder} from './usecases/invoke-add-user-reminder';
 import {invokeRemoveUserReminder} from './usecases/invoke-remove-user-reminder';
+import {add} from 'date-fns';
+
+const isValidChannelReaction = (event: ReactionAddedEvent | ReactionRemovedEvent) =>
+  event.item.type === 'message'
+  && event.reaction.startsWith('+1') // Makes sure the reaction is one of the +1 variants
+  && (event.item.channel.startsWith('C') || event.item.channel.startsWith('G')) // New channels start with C, old start with G
+  && add(new Date(parseFloat(event.item.ts) * 1000), {weeks: 2}) >= new Date(); // If the event is less than 2 weeks old
 
 const eventCallbackHandler = (payload: SlackCallbackEvent) => {
   switch (payload.event.type) {
     case 'reaction_added': {
       // Check if it's an event on our user's post
       const userId = payload.authorizations?.find(auth => auth.is_bot)?.user_id;
-      if (userId === payload.event.item_user && payload.event.item.type === 'message'
-      && payload.event.reaction.startsWith('+1') && (payload.event.item.channel.startsWith('C') || payload.event.item.channel.startsWith('G'))) {
-        return invokeAddUserReminder(payload.event.item.channel, payload.event.user, payload.event.item.ts);
+      if (userId === payload.event.item_user && isValidChannelReaction(payload.event)) {
+        return invokeAddUserReminder(
+          (payload.event.item as ReactionMessageItem).channel,
+          payload.event.user,
+          (payload.event.item as ReactionMessageItem).ts,
+        );
       }
 
       return;
@@ -18,9 +29,12 @@ const eventCallbackHandler = (payload: SlackCallbackEvent) => {
 
     case 'reaction_removed': {
       const userId = payload.authorizations?.find(auth => auth.is_bot)?.user_id;
-      if (userId === payload.event.item_user && payload.event.item.type === 'message'
-      && payload.event.reaction.startsWith('+1') && (payload.event.item.channel.startsWith('C') || payload.event.item.channel.startsWith('G'))) {
-        return invokeRemoveUserReminder(payload.event.item.channel, payload.event.user, payload.event.item.ts);
+      if (userId === payload.event.item_user && isValidChannelReaction(payload.event)) {
+        return invokeRemoveUserReminder(
+          (payload.event.item as ReactionMessageItem).channel,
+          payload.event.user,
+          (payload.event.item as ReactionMessageItem).ts,
+        );
       }
 
       return;
