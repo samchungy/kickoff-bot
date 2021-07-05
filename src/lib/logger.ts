0bfd05b111ade59995de199939875d6b07246d17
Sam Chung <samc@seek.com.au>
2021-07-06 00:37:00 +1000

    fix: correct default log level

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,7 +1,7 @@
 import pino, {PinoLambdaLogger} from 'pino-lambda';
 
 const logger: PinoLambdaLogger = pino({
-  level: process.env.LOG_LEVEL || '',
+  level: process.env.LOG_LEVEL || 'info',
   ...(process.env.NODE_ENV === 'local' && {
     prettyPrint: {colorize: true, translateTime: 'SYS:'},
   }),
