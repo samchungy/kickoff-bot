ce28cb77f8ceb072fa94a83f7fc5ab90c5fe3a36
Sam Chung <samc@seek.com.au>
2021-07-06 20:05:55 +1000

    feat: add post kickoff unit tests

---
+++ src/functions/post-kickoff/usecases/post-kickoff.test.ts
@@ -0,0 +1,109 @@
+import {InvokeCommandOutput} from '@aws-sdk/client-lambda';
+import {ChatPostMessageResponse, ErrorCode, WebAPIPlatformError} from '@slack/web-api';
+import {KickoffEvent} from 'domain/events';
+
+import {invokeAsync} from 'infrastructure/lambda-Interface';
+import {sendMessage} from 'infrastructure/slack-interface';
+import {putKickoff} from 'infrastructure/storage/kickoff-interface';
+import {logger} from 'lib';
+import {mocked} from 'ts-jest/utils';
+import {postKickoff} from './post-kickoff';
+
+jest.mock('infrastructure/lambda-Interface');
+jest.mock('infrastructure/slack-interface');
+jest.mock('infrastructure/storage/kickoff-interface');
+jest.mock('lib');
+
+const kickoffEvent: KickoffEvent = {
+  channelId: 'C025RNKNB28',
+  date: '2021-01-28',
+  description: 'test kickoff',
+  time: '20:30',
+  timezone: 'Australia/Melbourne',
+  userId: 'URVUTD7UP',
+  viewId: 'VMHU10V25',
+  zoom: 'https://seek.zoom.us/j/2089361925?pwd=test',
+};
+
+const samplePostMessageResponse: ChatPostMessageResponse = {
+  ok: true,
+  channel: 'C1H9RESGL',
+  ts: '1503435956.000247',
+  message: {
+    text: 'Here\'s a message for you',
+    username: 'ecto1',
+    bot_id: 'B19LU7CSY',
+    attachments: [
+      {
+        text: 'This is an attachment',
+        id: 1,
+        fallback: 'This is an attachment\'s fallback',
+      },
+    ],
+    type: 'message',
+    subtype: 'bot_message',
+    ts: '1503435956.000247',
+  },
+};
+
+const asyncInvokeResults: InvokeCommandOutput = {
+  $metadata: {},
+  StatusCode: 200,
+};
+
+beforeEach(() => {
+  jest.resetAllMocks();
+  mocked(sendMessage).mockResolvedValue(samplePostMessageResponse);
+  mocked(putKickoff).mockResolvedValue();
+  mocked(invokeAsync).mockResolvedValue(asyncInvokeResults);
+});
+
+it('should call the slack interface to create an new post', async () => {
+  const expectedText = '<@URVUTD7UP> is kicking off *test kickoff * at *<!date^1611826200^{date_short_pretty} - {time}|2021-01-28 09:30am Australian Eastern Daylight Time>*';
+
+  await expect(postKickoff(kickoffEvent)).resolves.toBeUndefined();
+
+  expect(sendMessage).toBeCalledWith(kickoffEvent.channelId, expectedText, expect.any(Object));
+});
+
+it('should call the kickoff interface to store the kickoff metadata', async () => {
+  await expect(postKickoff(kickoffEvent)).resolves.toBeUndefined();
+
+  expect(putKickoff).toBeCalledWith(kickoffEvent.channelId, samplePostMessageResponse.ts, 1611826200, kickoffEvent.userId);
+});
+
+it('should call to async invoke the add user reminder function', async () => {
+  await expect(postKickoff(kickoffEvent)).resolves.toBeUndefined();
+  expect(invokeAsync).toBeCalledWith({functionName: 'add-user-reminder', payload: {
+    channelId: kickoffEvent.channelId,
+    ts: samplePostMessageResponse.ts,
+    userId: kickoffEvent.userId,
+  }});
+});
+
+it('should attempt to send a Slack error message if the function fails to post', async () => {
+  const slackPlatformError: WebAPIPlatformError = {
+    code: ErrorCode.PlatformError,
+    data: {
+      ok: false,
+      error: 'not_in_channel',
+    },
+    name: ErrorCode.PlatformError,
+    message: 'not_in_channel',
+  };
+  mocked(sendMessage).mockRejectedValueOnce(slackPlatformError);
+  mocked(sendMessage).mockResolvedValueOnce(samplePostMessageResponse);
+
+  const expectedText = ':information_source: Failed to post a kickoff to <#C025RNKNB28>. Please run `/invite @kickoff` in the channel and click the retry button below.';
+
+  await expect(postKickoff(kickoffEvent)).resolves.toBeUndefined();
+  expect(sendMessage).lastCalledWith(kickoffEvent.userId, expectedText, expect.any(Object));
+});
+
+it('should log and throw an error when store fails', async () => {
+  const error = new Error();
+  mocked(putKickoff).mockRejectedValue(error);
+
+  await expect(postKickoff(kickoffEvent)).rejects.toThrowError(error);
+  expect(logger.error).toBeCalled();
+});
