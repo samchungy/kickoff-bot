36bbba801005d196565075bd6201739948589253
Sam Chung <samc@seek.com.au>
2021-07-10 15:39:17 +1000

    chore: remove unusesd file

--- src/functions/slack-actions/usecases/respond.ts
+++
@@ -1,9 +0,0 @@
-import {post} from 'infrastructure/http-gateway';
-
-const respond = async (responseUrl: string, text: string) => await post(responseUrl, {
-  text,
-  response_type: 'ephemeral',
-  replace_original: false,
-});
-
-export {respond};
